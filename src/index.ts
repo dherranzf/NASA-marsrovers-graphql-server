42cfb49eb2857a768c20ce03c5acc25016eceb8e
dherranzf <dhfdev14@gmail.com>
2025-03-26 22:49:04 +0100

    Add Apollo Server setup and Mars photo models

---
+++ src/index.ts
@@ -0,0 +1,25 @@
+import { ApolloServer } from "@apollo/server";
+import { startStandaloneServer } from "@apollo/server/standalone";
+import { typeDefs } from "./schema";
+import { resolvers } from "./resolvers";
+import { NasaMarsAPI } from "./datasources/nasa-mars-api";
+
+async function startApolloServer() {
+  const server = new ApolloServer({ typeDefs, resolvers });
+  const { url } = await startStandaloneServer(server, {
+    context: async () => {
+      const { cache } = server;
+      return {
+        dataSources: {
+          marsAPI: new NasaMarsAPI({ cache }),
+        },
+      };
+    },
+  });
+  console.log(`
+    🚀  Server is running!
+    📭  Query at ${url}
+  `);
+}
+
+startApolloServer();
---
+++ src/models.ts
@@ -0,0 +1,25 @@
+export type MarsPhotoModel = {
+  id: string;
+  sol: string;
+  camera: Camera;
+  img_src: string;
+  length: number;
+  earth_date: string;
+  number_of_views: number;
+  rover: Rover;
+};
+
+export type Camera = {
+  id: string;
+  name: string;
+  rover_id: number;
+  full_name: string;
+};
+
+export type Rover = {
+  id: string;
+  name: string;
+  landing_date: number;
+  launch_date: number;
+  status: number;
+}
