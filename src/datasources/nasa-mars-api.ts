e009fff4d19dcd41b30450b9e90aac1683345d36
dherranzf <dhfdev14@gmail.com>
2025-03-27 14:25:33 +0100

    Refactor NasaMarsAPI data source methods to use async/await for improved readability and error handling

--- src/datasources/nasa-mars-api.ts
+++ src/datasources/nasa-mars-api.ts
@@ -1,20 +1,23 @@
 import { RESTDataSource } from "@apollo/datasource-rest";
-import { MarsPhotoModel } from "../models"
+import { MarsPhotoModel } from "../models";
 
 export class NasaMarsAPI extends RESTDataSource {
   // the API NASA Mars Rover Photos is here
   baseURL = "https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos";
   
-  getMarsPhotos() {
-    return this.get<MarsPhotoModel[]>("photos");
+  // Fetch all Mars photos
+  async getMarsPhotos(): Promise<MarsPhotoModel[]> {
+    return await this.get<MarsPhotoModel[]>("photos");
   }
 
-  getMarsPhoto(photoId: string) {
-    const photos = this.get<MarsPhotoModel[]>("photos");
+  // Fetch a specific Mars photo by ID
+  async getMarsPhoto(photoId: string): Promise<MarsPhotoModel | undefined> {
+    const photos = await this.get<MarsPhotoModel[]>("photos");
     return photos.find(photo => photo.id === photoId);
   }
 
-  incrementMarsPhotoViews(photoId: string) {
-    return this.patch<MarsPhotoModel>(`photos/${photoId}/numberOfViews`);
+  // Increment the number of views for a specific Mars photo
+  async incrementMarsPhotoViews(photoId: string): Promise<MarsPhotoModel> {
+    return await this.patch<MarsPhotoModel>(`photos/${photoId}/numberOfViews`);
   }
 }
