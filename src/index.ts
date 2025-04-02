eb192cee813f88c9c0daf7ac0c951d37289ee3f4
dherranzf <dhfdev14@gmail.com>
2025-04-02 18:51:50 +0200

    Configure Apollo Server to listen on a specified port from environment variables

--- src/index.ts
+++ src/index.ts
@@ -3,11 +3,12 @@ import { startStandaloneServer } from "@apollo/server/standalone";
 import { typeDefs } from "./schema";
 import { resolvers } from "./resolvers/resolvers";
 import { NasaMarsAPI } from "./datasources/nasa-mars-api";
-import 'dotenv/config'; // Carga automáticamente las variables de entorno desde el archivo .env
+import 'dotenv/config'; // Automatically loads environment variables from the .env file
 
 async function startApolloServer() {
   require('dotenv').config();
   const server = new ApolloServer({ typeDefs, resolvers });
+  const port = Number(process.env.PORT) || 4000; // Convert the port to a number or default to 4000
   const { url } = await startStandaloneServer(server, {
     context: async () => {
       const { cache } = server;
@@ -17,6 +18,7 @@ async function startApolloServer() {
         },
       };
     },
+    listen: { port },
   });
   console.log(`
     🚀  Server is running!
